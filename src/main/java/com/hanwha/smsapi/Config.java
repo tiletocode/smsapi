1787e4aa18aca6494ea51df5641c878511a8fd63
tiletocode <jin880@naver.com>
2023-12-19 19:21:47 +0900

    프로퍼티 문자 인코딩 수정, 이벤트문구 전처리

--- src/main/java/com/hanwha/smsapi/Config.java
+++ src/main/java/com/hanwha/smsapi/Config.java
@@ -1,7 +1,8 @@
 package com.hanwha.smsapi;
 
 import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,11 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 public class Config {
     private Properties props = new Properties();
     private static Config instance = new Config();
+
     private Config() {
         String propsPath = System.getProperty("config.path", "config/config.properties");
         try {
-            InputStream is = new FileInputStream(propsPath);
-            props.load(is);
+            // Specify UTF-8 encoding
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(propsPath), StandardCharsets.UTF_8);
+            props.load(reader);
         } catch (Exception e) {
             log.error("Failed to read the config file.", e);
         }
--- src/main/java/com/hanwha/smsapi/SendJson.java
+++ src/main/java/com/hanwha/smsapi/SendJson.java
@@ -98,7 +98,8 @@ public class SendJson {
         // Payload 정의
         String sendCont = "1";
         String ntfcKindCode = config.getString("payload.ntfckindcode", "ZAC9001");
-        String jobMsgeCntn = dto.getMessage();
+        String prefixCntn = config.getString("payload.prefixcntn", "[Whatap-모니터링 알림]\\n");
+        String jobMsgeCntn = prefixCntn + dto.getMessage();
         String sndeDeptCode = config.getString("payload.sndedeptcode", "00025");
         String ntfcTmplCode = config.getString("payload.ntfctmplcode", "AZAC000015");
         String btchPrcsYn = config.getString("payload.btchprcsyn", "1");
@@ -181,7 +182,7 @@ public class SendJson {
                     dataJsonNode.set("header", headerNode);
                     dataJsonNode.set("payload", payloadNode);
 
-                    String finalOutput = dataJsonNode.toPrettyString();
+                    String finalOutput = dataJsonNode.toString();
 
                     // OkHttp를 사용한 POST 요청
                     RequestBody requestBody = RequestBody.create(finalOutput, MediaType.get("application/json"));
